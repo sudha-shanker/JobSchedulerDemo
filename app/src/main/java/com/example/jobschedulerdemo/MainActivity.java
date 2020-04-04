9df4b0efe20a08fb567ba929b97d51b17cc46a69
Sudha Shanker Prasad <anubis1191@gmail.com>
2020-04-04 14:28:32 +0530

    JobScheduler

---
+++ app/src/main/java/com/example/jobschedulerdemo/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.jobschedulerdemo;
+
+
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+public class MainActivity extends AppCompatActivity {
+    JobInfo jobInfo;
+    JobScheduler jobScheduler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void startJob(View view)
+    {
+
+       jobScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+       ComponentName componentName = new ComponentName(this,MyJobService.class);
+
+       
+       JobInfo.Builder builder = new JobInfo.Builder(123, componentName);
+        builder.setMinimumLatency(1 * 1000);
+        builder.setOverrideDeadline(3 * 1000);
+        builder.setPersisted(true);
+        builder.setRequiredNetworkType(jobInfo.NETWORK_TYPE_ANY);
+        builder.setRequiresCharging(false);
+        jobScheduler.schedule(builder.build());
+    }
+
+    public void stopJob(View view)
+    {
+        if(jobScheduler != null)
+        {
+            jobScheduler.cancel(123);
+            jobScheduler = null;
+
+         Toast.makeText(this,"Job Cancelled", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/jobschedulerdemo/MyJobService.java
@@ -0,0 +1,34 @@
+package com.example.jobschedulerdemo;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+import static android.content.ContentValues.TAG;
+
+public class MyJobService extends JobService {
+
+    @Override
+    public boolean onStartJob(JobParameters jobParameters) {
+        Log.i(TAG, "onStartJob:");
+       Intent intent = new Intent(MyJobService.this,
+               AlarmManagerBroadcast.class);
+       PendingIntent pendingIntent = PendingIntent.getBroadcast
+               (MyJobService.this,234,intent,0 );
+       AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+       alarmManager.set(AlarmManager.RTC_WAKEUP,System.currentTimeMillis(),
+               pendingIntent);
+       Toast.makeText(MyJobService.this,"Alarm Set ",Toast.LENGTH_LONG).show();
+        return true;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters jobParameters) {
+        Log.i(TAG, "onStopJob:");
+        return true;
+    }
+}
